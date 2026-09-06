79e5766bd69220fe1f2a5cfa8c9c72c63163ec54
viniciusshenri96 <vinicius_henrique96@hotmail.com>
2026-09-06 11:24:50 -0300

    :sparkles: feat:  Implementing reveal sections using the DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
 "use stricts";
+
+///////////////////////////////////////
+// Reveal sections
+const allSection = document.querySelectorAll(".revealSection");
+
+const revealSection = function (entries, observer) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove("section--hidden");
+    console.log(observer);
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSection.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
